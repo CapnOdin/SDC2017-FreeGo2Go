8cf4b5aeb39220f142359da50b4ca3b8ef9b97fb
deltager <github@software.unf.dk>
2017-07-06 11:31:25 +0200

    Updated Structure

--- app/src/main/java/software/unf/dk/freego2go/MainActivity.java
+++ app/src/main/java/software/unf/dk/freego2go/MainActivity.java
@@ -3,11 +3,41 @@ package software.unf.dk.freego2go;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.stream.IntStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nigiri();
+
+
     }
+
+    //Nigiri Colorselection()
+    protected void nigiri(){
+        double number = Math.random();
+    }
+
+    //Board Generator()
+
+    //Tile Search()
+
+    //State Switch()
+
+
+    /*Should have
+     *
+      * Scorecheck()
+      *
+      * Liberties()
+      *
+      * Capture Area Search()
+      *
+      * Hook up Graphics
+      *
+      * */
 }
---
+++ app/src/main/java/software/unf/dk/freego2go/Variables.java
@@ -0,0 +1,18 @@
+package software.unf.dk.freego2go;
+
+/**
+ * Created by deltager on 06-07-17.
+ */
+
+public class Variables {
+    public static boolean turn;
+    public static int boardsizeModifier = 9;
+    public static int amountOfWhite = 0;
+    public static int amountOfBlack = 0;
+    public static int amountOfTurns = 0;
+    public static int pointsOfWhite = 0;
+    public static int pointsOfBlack = 0;
+
+
+
+}
